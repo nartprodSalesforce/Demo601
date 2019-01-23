600fde52ff5e3c3e3d7f47ef0c59550f15f00f3e
Stuart <swerner@salesforce.com>
2019-01-23 10:01:00 -0500

    Show/Hide with Pictures

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,12 @@
+declare module "@salesforce/resourceUrl/Man" {
+    var Man: string;
+    export default Man;
+}
+declare module "@salesforce/resourceUrl/Pet" {
+    var Pet: string;
+    export default Pet;
+}
+declare module "@salesforce/resourceUrl/Woman" {
+    var Woman: string;
+    export default Woman;
+}
--- force-app/main/default/aura/HideShow/HideShowController.js
+++ force-app/main/default/aura/HideShow/HideShowController.js
@@ -4,7 +4,7 @@
         $A.util.toggleClass(el, 'slds-hide');
     },
     clickHidden: function (component, event, helper) {
-        var el = component.find('response1');
+        var el = component.find('response');
         $A.util.toggleClass(el, 'slds-hidden');
     }
 })
