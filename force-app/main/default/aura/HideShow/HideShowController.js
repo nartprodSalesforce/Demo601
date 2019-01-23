59472934acba844a099a317592a168d900a3356a
Stuart <swerner@salesforce.com>
2019-01-23 09:47:47 -0500

    Show/Hide Phase 1

---
+++ force-app/main/default/aura/HideShow/HideShowController.js
@@ -0,0 +1,10 @@
+({
+    clickHide: function (component, event, helper) {
+        var el = component.find('response');
+        $A.util.toggleClass(el, 'slds-hide');
+    },
+    clickHidden: function (component, event, helper) {
+        var el = component.find('response1');
+        $A.util.toggleClass(el, 'slds-hidden');
+    }
+})
