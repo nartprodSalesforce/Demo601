925ea20095cf90aca670622a2c68b4aa094c1617
Stuart <swerner@salesforce.com>
2019-01-24 18:00:02 -0500

    Clean Up Events Dynamic Select

--- force-app/main/default/aura/AddTextToList/AddTextToListController.js
+++ force-app/main/default/aura/AddTextToList/AddTextToListController.js
@@ -1,20 +1,10 @@
 ({
-    handleClick : function (component, event, helper) {
-        var myName = component.find("name").get("v.value");
-        var myValue = component.find("value").get("v.value");
-        var myNames = component.get("v.names");  
-        //console.log ("Before Push names[]" , myNames );
-        myNames.push({Name: myName, Value: myValue });
-        console.log ("After push names[]" , myNames );
-        component.set("v.names",myNames);  
-    },
 	onNameAdded: function(component, event, helper) {
         debugger;
         var myValue = event.getParam('newValue');;
         var myName = event.getParam('newName');
         var myNames = component.get("v.names");  
         myNames.push({Name: myName, Value: myValue });
-        console.log ("After push names[]" , myNames );
         component.set("v.names",myNames);
 	}
 })
