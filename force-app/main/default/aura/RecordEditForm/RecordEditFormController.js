f3d853c35e757a0e6ef7ae705597a255e1ba1a52
Stuart <swerner@salesforce.com>
2019-01-25 14:41:37 -0500

    LDS RecordEditForm

---
+++ force-app/main/default/aura/RecordEditForm/RecordEditFormController.js
@@ -0,0 +1,11 @@
+({
+    handleLoad : function(component, event, helper) {
+        component.set("v.showSpinner", "false");
+    },
+    handleSubmit: function(component, event, helper) {
+       // component.set("v.showSpinner", "false");
+    },
+    handleSuccess: function(component, event, helper) {
+        // component.set("v.showSpinner", "false");
+     }
+})
