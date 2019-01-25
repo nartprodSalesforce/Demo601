d1dba0fb1ae297781461d7582d17302473f84e28
Stuart <swerner@salesforce.com>
2019-01-25 09:23:50 -0500

    Phase 2 LDS Demo Grab Account ID

--- force-app/main/default/aura/AddAccountsToSelect/AddAccountsToSelectController.js
+++ force-app/main/default/aura/AddAccountsToSelect/AddAccountsToSelectController.js
@@ -2,5 +2,8 @@
     doInit: function (component, event, helper) {
         helper.onInit(component, event, helper);
 
+    },
+    onChange: function (component, event, helper) {
+        alert('Account ID: '+ component.find('select').get('v.value'));
     }
 })
